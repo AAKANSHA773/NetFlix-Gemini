f1ce6b41bb6afd156391bb27b3e9d07488e7b29c
AAKANSHA773 <aakanshapanwar01@gmail.com>
2026-01-05 18:46:16 +0530

    change the usemovietrailer

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,4 +1,4 @@
-import {  useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { API_OPTION } from "../utils/constant";
 import { useDispatch, useSelector } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSLice";
@@ -6,24 +6,39 @@ import { addTrailerVideo } from "../utils/movieSLice";
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
 
-   const trailerVideo = useSelector(store => store.movies.trailerVideo)
-  const getMovieVideo = async () => {
+  const trailerVideo = useSelector(
+    (store) => store.movies.trailerVideo
+  );
+
+  const getMovieVideo = useCallback(async () => {
+    if (!movieId) return;
+
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/" +
         movieId +
         "/videos?language=en-US",
       API_OPTION
     );
+
     const json = await data.json();
 
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.lenght ? filterData[0] : json.results[0];
-    
+    const filterData = json.results?.filter(
+      (video) => video.type === "Trailer"
+    );
+
+    const trailer =
+      filterData?.length > 0
+        ? filterData[0]
+        : json.results?.[0];
+
     dispatch(addTrailerVideo(trailer));
-  };
+  }, [movieId, dispatch]);
+
   useEffect(() => {
-    if(!trailerVideo)
-    getMovieVideo();
-  }, []);
+    if (!trailerVideo) {
+      getMovieVideo();
+    }
+  }, [getMovieVideo, trailerVideo]);
 };
+
 export default useMovieTrailer;
